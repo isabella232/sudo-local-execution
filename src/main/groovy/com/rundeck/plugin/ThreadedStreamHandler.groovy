dfbd5d319974a5246d7e06d78df7afa5a7d3af12
Luis Toledo <luis@variacode.com>
2019-07-19 09:19:51 -0400

    removing password prompt string from output

--- src/main/groovy/com/rundeck/plugin/ThreadedStreamHandler.groovy
+++ src/main/groovy/com/rundeck/plugin/ThreadedStreamHandler.groovy
@@ -2,6 +2,9 @@ package com.rundeck.plugin
 
 import com.dtolabs.rundeck.core.execution.ExecutionLogger
 
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
 class ThreadedStreamHandler extends Thread{
     InputStream inputStream
     String adminPassword
@@ -10,6 +13,8 @@ class ThreadedStreamHandler extends Thread{
     boolean sudoIsRequested = false
     ExecutionLogger logger
     boolean isErrorStream=false
+
+    List<String> sudoPatterns = ["^Password:(.*)","^\\[sudo\\] password for ([^:]+):\\s(.*)"]
 
     ThreadedStreamHandler(InputStream inputStream,
                           OutputStream outputStream,
@@ -29,21 +34,23 @@ class ThreadedStreamHandler extends Thread{
 
     void run()
     {
-        if (sudoIsRequested)
-        {
-            printWriter.println(adminPassword)
-            printWriter.flush()
-        }
-
         BufferedReader bufferedReader = null
         try
         {
+
+            if (sudoIsRequested)
+            {
+                printWriter.println(adminPassword)
+                printWriter.flush()
+            }
+
             bufferedReader = new BufferedReader(new InputStreamReader(inputStream))
             bufferedReader.eachLine {line->
 
                 if(isErrorStream){
-                    if(line.contains("Password:") || line.contains("[sudo] password")){
-                        String message = line.replace("Password:","")
+
+                    if(checkSudoPattern(line)){
+                        String message = replacePattern(line)
                         if(message){
                             logger.log(0,  message)
                         }
@@ -78,6 +85,33 @@ class ThreadedStreamHandler extends Thread{
         }
     }
 
+    boolean checkSudoPattern(def line){
+        boolean status = sudoPatterns.stream().anyMatch({pattern->
+            Pattern.compile(pattern).matcher(line).matches()
+        })
+        status
+    }
 
+    String replacePattern(def line){
+        String replace = line
+        for(String pattern:sudoPatterns){
+            if(Pattern.compile(pattern).matcher(line).matches()){
+                Matcher matcher = Pattern.compile(pattern).matcher(line)
+                int count = matcher.groupCount()
+
+                if (matcher.find()) {
+                    String find = matcher.group(count)
+                    if(find.isEmpty()){
+                        replace=null
+                    }else{
+                        replace=find
+                    }
+                }
+                return replace
+            }
+
+        }
+        replace
+    }
 
 }
